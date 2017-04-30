a3a6f732b33fbd6ee086f7a465273bef07965965
lucas yi <423188112@qq.com>
2017-04-30 16:36:42 +1000

    add project design

---
+++ src/entity/product.java
@@ -0,0 +1,7 @@
+package entity;
+
+/**
+ * Created by Yi on 4/30/2017.
+ */
+public class product {
+}
---
+++ src/user_interface/MainPage.java
@@ -0,0 +1,7 @@
+package user_interface;
+
+/**
+ * Created by Yi on 4/30/2017.
+ */
+public class MainPage {
+}
